11ebba5ccc67d249232a661f033222e92977153a
Rachel Wong <hello@rachelwonghw.com>
2021-09-19 04:48:02 -0400

    wrote 2 divs that represent the buttons for toggling between 2 tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Logo from './components/Logo';
-import './App.css';
+import './App.scss';
 import Analytics from './pages/Analytics';
 import History from './pages/History';
 import Transcript from './pages/Transcript';
@@ -8,9 +8,13 @@ import Transcript from './pages/Transcript';
 //TODO: add nav tab between history and analytics
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
+    <div className="App">
       <Logo className="App__logo" />
+      <div className="App__tab">
+        <div className="App__tab__button App__tab__button--active">History</div>
+        <div className="App__tab__button App__tab__button--inactive App__tab__button--right">Analytics</div>
+      </div>
+      <BrowserRouter>
         <Switch>
           <Route exact path="/">
             <Redirect to="/home" />
@@ -25,8 +29,8 @@ const App = () => {
             <Transcript date={1631910840000} />
           </Route>
         </Switch>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
