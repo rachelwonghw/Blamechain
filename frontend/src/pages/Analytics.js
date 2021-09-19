eddcfa52dea8dde4acb71b6fadaf3518b2dd01cd
Rachel Wong <hello@rachelwonghw.com>
2021-09-18 20:43:48 -0400

    added piechart package

--- frontend/src/pages/Analytics.js
+++ frontend/src/pages/Analytics.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
 
 const Analytics = () => {
   return (
-    <div>analytics</div>
+    <div>
+      <h1>analytics</h1>
+      <PieChart
+        paddingAngle={2}
+        data={[
+          { title: 'Bob', value: 55, color: '#BA4863' },
+          { title: 'Alice', value: 35, color: '#F2D9DB' },
+          { title: 'Mallory', value: 10, color: '#E6A2AA' },
+        ]
+      }
+      />
+    </div>
   );
 }
 
